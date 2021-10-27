893211c875069685b2332a0e0c4b36a4a26d7934
Archan Lagoo <aklagoo@gmail.com>
2021-10-26 21:15:35 -0700

    Added test for read

--- test/Model.test.js
+++ test/Model.test.js
@@ -3,23 +3,32 @@ const { Model } = require('../src/model.js');
 
 let model;
 describe('Model', () => {
-    before(() => {
-        model = new Model();
-    });
+  before(() => {
+    model = new Model();
+  });
 
-    describe("#exists()", () => {
-        it('should return true if key exists', (done) => {
-            model.exists("test_exists").then((value) => {
-                expect(value).to.be.true;
-                done();
-            });
-        });
-        it('should return false if key does not exist', (done) => {
-            // TODO Fix test return check
-            model.exists("test_does_not_exist").then((value) => {
-                expect(value).to.be.false;
-                done();
-            });
-        });
+  describe("#exists()", () => {
+    it('should return true if key exists', (done) => {
+      // TODO Fix test return check
+      model.exists("test_exists").then((value) => {
+        expect(value).to.be.true;
+        done();
+      });
     });
+    it('should return false if key does not exist', (done) => {
+      // TODO Fix test return check
+      model.exists("test_does_not_exist").then((value) => {
+        expect(value).to.be.false;
+        done();
+      });
+    });
+  });
+
+  describe("#read()", () => {
+    it('should return the correct value if the key exists.', (done) => {
+      model.read("test_read").then((value) => {
+        expect(value).to.equal("Hello world!");
+      });
+    });
+  });
 });
