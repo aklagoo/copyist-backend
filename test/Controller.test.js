54771af4b5d3ab1d44ae84f5ffc5ee7a0e987e63
Archan Lagoo <aklagoo@gmail.com>
2021-10-27 20:08:43 -0700

    Fixed indentation

--- test/Controller.test.js
+++ test/Controller.test.js
@@ -9,36 +9,36 @@ const connectRoomIDInvalid = 'test_UUIDInvalid';
 
 let controller;
 describe('Controller', () => {
-    /* Initialize the controller. */
-    before(() => {
-        controller = new Controller();
-    });
+  /* Initialize the controller. */
+  before(() => {
+    controller = new Controller();
+  });
 
-    /* All tests */
-    describe('#connect', () => {
-        it('should return the same roomID if it exists.', (done) => {
-            controller.connect(connectRoomIDValid).then((data) => {
-                expect(data.roomID).to.equal(connectRoomIDValid);
-                done();
-            });
-        });
-        it('should return the same message if the roomID exists.', (done) => {
-            controller.connect(connectRoomIDValid).then((data) => {
-                expect(data.message).to.equal(connectMessageValid);
-                done();
-            });
-        });
-        it('should return a valid UUID if the roomID is invalid.', (done) => {
-            controller.connect(connectRoomIDInvalid).then((data) => {
-                expect(validate(data.roomID)).to.be.true;
-                done();
-            });
-        });
-        it('should return an empty message if the roomID is invalid.', (done) => {
-            controller.connect(connectRoomIDInvalid).then((data) => {
-                expect(data.message).to.equal('');
-                done();
-            });
-        });
+  /* All tests */
+  describe('#connect', () => {
+    it('should return the same roomID if it exists.', (done) => {
+      controller.connect(connectRoomIDValid).then((data) => {
+        expect(data.roomID).to.equal(connectRoomIDValid);
+        done();
+      });
     });
+    it('should return the same message if the roomID exists.', (done) => {
+      controller.connect(connectRoomIDValid).then((data) => {
+        expect(data.message).to.equal(connectMessageValid);
+        done();
+      });
+    });
+    it('should return a valid UUID if the roomID is invalid.', (done) => {
+      controller.connect(connectRoomIDInvalid).then((data) => {
+        expect(validate(data.roomID)).to.be.true;
+        done();
+      });
+    });
+    it('should return an empty message if the roomID is invalid.', (done) => {
+      controller.connect(connectRoomIDInvalid).then((data) => {
+        expect(data.message).to.equal('');
+        done();
+      });
+    });
+  });
 });
