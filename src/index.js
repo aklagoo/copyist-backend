6c151cece461cf04356d5f76b0b54e0e7aab8803
Archan Lagoo <aklagoo@gmail.com>
2021-10-28 18:51:14 -0700

    Added socket event handlers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 /* Imports */
 const conf = require('./conf.json');
+const { Controller } = require('./controller');
 const express = require('express');
 const http = require('http');
 const { Server: SocketServer } = require('socket.io');
 
-/* Create an Express app and a socket.io server. */
+/* Create an Express app, a socket.io server and a controller. */
 const app = express();
 const server = http.createServer(app);
-const io = SocketServer(server, {
+const io = new SocketServer(server, {
   cors: {
     origin: "*",
   }
 });
+const controller = new Controller();
 
 /* Redirect user to client URL. */
 app.get('/', (req, res) => {
@@ -25,6 +27,36 @@ server.listen(conf.SELF_PORT, () => {
 
 /* Socket.io server setup */
 io.on('connection', (socket) => {
+  let roomID = socket.handshake.query.roomID;
   console.log('Received roomID: ' + socket.handshake.query.roomID);
-  socket.emit('roomID', '0');
+
+  // Join a room and return info.
+  controller.connect(roomID).then((data) => {
+    // Update roomID.
+    roomID = data.roomID;
+    socket.join(roomID);
+
+    // Emit connection information.
+    socket.emit('connected', data);
+  }).catch((err) => {
+    // Send a connection error, if any.
+    console.log(err);
+    socket.emit('connection_error', 'Unable to connect. Internal server error.');
+
+    // Disconnect
+    socket.disconnect(true);
+  });
+
+  // Event handler
+  socket.on('update', (message) => {
+    // Attempt an update.
+    controller.update(roomID, message).then(() => {
+      // If successful, broadcast the update to the room.
+      socket.to(roomID).emit('update', message);
+    }).catch((err) => {
+      // If failed, send an updation error.
+      console.log(err);
+      socket.emit('updation_error', 'Unable to update message. Internal server error.');
+    });
+  })
 });
